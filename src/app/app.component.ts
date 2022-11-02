bb096a2576499fa8a2729b86e0713bb47d6f6d94
kittinan-k <kittinan-k@kyocera.co.th>
2022-11-02 12:22:14 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -14,22 +15,38 @@ export class AppComponent {
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+    private swUpdate: SwUpdate
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
     this.items = [
       {
-        path:'',
-        icon:'home',
-        title:'Home'
+        path: '',
+        icon: 'home',
+        title: 'Home'
       },
       {
-        path:'',
-        icon:'feed',
-        title:'request'
+        path: '',
+        icon: 'feed',
+        title: 'request'
       }
     ]
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+          if(confirm("New version available. Load New Version?")) {
+
+              window.location.reload();
+          }
+      });
+  }
   }
 
   ngOnDestroy(): void {
