bc8457342ff9ebcd840ec91cac8c8ab43a9d20fc
kittinan-k <kittinan-k@kyocera.co.th>
2023-05-22 15:56:31 +0700

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // API: 'http://localhost:4031',
+  API: 'http://localhost:4031',
   // API: 'http://10.200.90.152:9999',
-  API: 'http://10.200.90.152:4031',
+  // API: 'http://10.200.90.152:4031',
   BASE: '/',
 };
 
